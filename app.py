5334061fd9eb9567ffb60244e9aad49dad829084
rawkintrevo <trevor.d.grant@gmail.com>
2021-11-01 09:37:11 -0500

    push before adding scraper tech

--- app.py
+++ app.py
@@ -27,22 +27,31 @@ TOWNSHIP = args.township
 NEIGHBORHOOD_CODE = args.nc
 PROPERTY_CLASS = args.pc
 
-driver = webdriver.Firefox()
+TOWNSHIP="West Chicago"
+NEIGHBORHOOD_CODE= "20"
+PROPERTY_CLASS= "211"
+
+
+driver = webdriver.Firefox(executable_path="./geckodriver")
 
 driver.wait = WebDriverWait(driver, 10)
 driver.get(start_url)
 # Open "Search Neighborhood" Accordion
-driver.find_elements_by_class_name("accordion-group")[-1].click()
+# driver.find_elements_by_class_name("accordion-group")[-1].click()
 # Fill out the form.
-select_township = Select(driver.find_element_by_xpath('//*[@id="ctl00_phArticle_ctlPropertySearch_ctlNeighborhoodSearch_ddlTownship"]'))
+# select_township = Select(driver.find_element_by_xpath('//*[@id="ctl00_phArticle_ctlPropertySearch_ctlNeighborhoodSearch_ddlTownship"]'))
+select_township = Select(driver.find_element_by_id('edit-township'))
 select_township.select_by_visible_text(TOWNSHIP)
 sleep(0.5)
-select_neighborhood = Select(driver.find_element_by_xpath('//*[@id="ctl00_phArticle_ctlPropertySearch_ctlNeighborhoodSearch_ddlNeighborhood"]'))
+select_neighborhood = Select(driver.find_element_by_id('edit-neighborhood--BdfHtSp1KO4'))
 select_neighborhood.select_by_value(NEIGHBORHOOD_CODE)
-select_prop_type = Select(driver.find_element_by_xpath('//*[@id="ctl00_phArticle_ctlPropertySearch_ctlNeighborhoodSearch_ddlClasification"]'))
+select_prop_type = Select(driver.find_element_by_id('edit-property-class'))
 select_prop_type.select_by_value(PROPERTY_CLASS)
-driver.find_element_by_xpath('//*[@id="ctl00_phArticle_ctlPropertySearch_ctlNeighborhoodSearch_btnSearch"]').click()
-sleep(3)
+## get the captcha
+#
+
+cookies = driver.get_cookies()
+
 
 output = []
 nextExists=True
@@ -50,76 +59,80 @@ page_n = 0
 while nextExists:
     print(f"Scraping page {page_n}")
     page_n += 1
-    sleep(1)
+    sleep(1.5)
     rows = driver.find_elements_by_xpath("//tr")
     r = rows[0]
     col_names = [c.text for c in r.find_elements(By.TAG_NAME, "th")]
     for r in rows[1:]:
         vals = r.find_elements(By.TAG_NAME, "td")
+        if len(vals) == 0:
+            break
         out = {col_names[i] : vals[i].text for i in range(0,len(col_names))}
-        out["value"] = int(out['Total Assessed Value'].replace(",","").replace("$",""))
+        out["value"] = int(out['TOTAL\nASSESSED\nVALUE'].replace(",","").replace("$",""))
         output.append(out)
     sleep(4)
-    if len(driver.find_elements_by_link_text("Next")) == 0:
+    pages = driver.find_elements_by_link_text(str(page_n+1))
+    if len(pages) > 0:
+        pages[0].click()
+    else:
         nextExists = False
-    driver.find_element_by_link_text("Next").click()
+    # if len(driver.find_elements_by_link_text("Next")) == 0:
+    #     nextExists = False
+    # driver.find_element_by_link_text("Next").click()
 
 # Create a CSV
 df = pd.DataFrame(output)
+df.to_csv('1312comps.csv')
 print(f"Successfully collected a list of {len(output)} properties- now fetching detailed information on each.")
 
+
 new_output = []
-start_adding = True # set to false for hot starts
-for ind, (addr,
-          build_value_str,
-          bld_sq_footage,
-          city,
-          class_code,
-          land_value,
-          nbrhd,
-          pin,
-          total_value,
-          unit,
-          value) in df.sort_values(by=["Unit", 'value']).iterrows():
+
+start_adding =True # True # set to false for hot starts
+for ind, (pin, addr, unit, city,
+          class_code, land_sq_footage, bld_sq_footage, land_val, bld_val, total_val, value) in df.sort_values(by=["UNIT", 'value']).iterrows():
     if start_adding == False:
         if new_output[-1]['pin'] == pin:
             start_adding = True
+            print('ok all caught up now')
         continue
-    url = "https://www.cookcountyassessor.com/Property.aspx?mode=details&pin=" + pin
+
+    url = "https://www.cookcountyassessor.com/pin/" + pin.replace('-', '')
     driver.get(url)
     sleep(1)
+    driver.find_elements_by_link_text("Characteristics")[0].click()
+
+    es = driver.find_elements_by_class_name('detail-row--detail')
     new_record = {
         'addr' : addr,
         'value' : value,
         'pin' : pin,
-        'sq_ft' : int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropInfoSqFt").text.replace(',', "")),
-        'land_val_2017': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblAsdValLandFirstPass").text.replace(",","")),
-        'bldg_val_2017': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblAsdValBldgFirstPass").text.replace(",","")),
-        'totl_val_2017':  int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblAsdValTotalFirstPass").text.replace(",","")),
-        'land_val_2016': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblAsdValLandCertified").text.replace(",","")),
-        'bldg_val_2016': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblAsdValBldgCertified").text.replace(",","")),
-        'totl_val_2016': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblAsdValTotalCertified").text.replace(",","")),
-        'mrkt_val_2017': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharMktValCurrYear").text.replace(",","").replace("$","")),
-        'mrkt_val_2016': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharMktValPrevYear").text.replace(",","").replace("$","")),
-        'res_type': driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharResType").text,
-        'res_use': driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharUse").text,
-        'apts': driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharApts").text,
-        'ext_constr': driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharExtConst").text,
-        'full_bath': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharFullBaths").text),
-        'half_bath': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharHalfBaths").text),
-        'basement': driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharBasement").text,
-        'attic': driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharAttic").text,
-        'cntl_air': driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharCentAir").text,
-        'fireplaces': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharFrpl").text),
-        'garage': driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharGarage").text,
-        'age': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharAge").text),
-        'bldg_sqft': int(driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharBldgSqFt").text.replace(",","")),
-        'assmt_pass': driver.find_element_by_id("ctl00_phArticle_ctlPropertyDetails_lblPropCharAsmtPass").text
+        'bld_sq_ft': bld_sq_footage,
+        'lnd_sq_ft': land_sq_footage,
+        'bld_val': bld_val,
+        'land_val': land_val,
+
     }
-    print(f"Collecting details on {new_record['addr']} (valued at ${new_record['value']}")
+    other_cols = [
+        'apts', #12
+        'ext_constr',
+        'full_bath',
+        'half_bath',
+        'basement',
+        'attic',
+        'cntl_air',
+        'fireplaces',
+        'garage',
+        'age',
+        'bldg_sqft',
+        'assmt_pass' #23
+        ]
+
+    new_record.update({other_cols[i -12 ] : es[i].text for i in range(12, min(len(es), 24))})
+    print(f"Collecting details on {new_record['addr']} (valued at ${new_record['value']})")
     new_output.append(new_record)
 
 df2 = pd.DataFrame(new_output)
 
 filename = f'{datetime.now().strftime("%Y-%m-%d")}-{TOWNSHIP}-{NEIGHBORHOOD_CODE} {PROPERTY_CLASS}.csv'
-df2.to_csv(f"data/{filename}", index=False)
+df2.to_csv(f"{filename}", index=False)
